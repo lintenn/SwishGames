0980eb80752af918fba2d2cb6b2146bc39e15f2c
Ángel Joaquín Rodríguez Mercado <angelrodriguezmercado95@gmail.com>
2022-03-20 21:42:13 +0100

    Update chat

--- frontend-react/src/vistas/chat/componentes/Chat.js
+++ frontend-react/src/vistas/chat/componentes/Chat.js
@@ -12,6 +12,7 @@ const Chat = ({nombre}) => {
     const [mensaje, setMensaje] = useState('')
     const [mensajes, setMensajes] = useState([])
     const [mensajesDESC, setMensajesDESC] = useState([])
+    const [users, setUsers] = useState([])
     const [receptor, setReceptor] = useState('')
     const divRef = useRef(null)
 
@@ -20,47 +21,59 @@ const Chat = ({nombre}) => {
     }, [nombre])
 
     useEffect(() => {
-        socket.on('ConectarUser', (nombre) => {
-            console.log('1')
-            if(receptor === nombre) {
-                console.log('2')
-                document.getElementById("offline").classList.add("ocultar");
-                document.getElementById("online").classList.add("mostrar");
-            }
-        })
-
-        return () => {socket.off()}
-    }, [nombre])
-
-    useEffect(() => {
-        socket.on('DesonectarUser', (nombre) => {
-            if(receptor === nombre) {
-                document.getElementById("online").classList.add("ocultar");
-                document.getElementById("offline").classList.add("mostrar");
-            }
-        })
-
-        return () => {socket.off()}
-    }, [nombre])
-
-    useEffect(() => {
         socket.on('mensajes', () => {
-            getMensajesOnePerson(receptor)
+            getMensajes()
         })
 
         return () => {socket.off()}
     }, [mensajes])
 
+    useEffect(() => {
+        socket.on('otroConectado', (nomb) => {
+            getUsers(receptor)
+        })
+
+        return () => {socket.off()}
+    }, [users])
+
+    useEffect(() => {
+        socket.on('otroDesconectado', (nomb) => {
+            getUsers(receptor)
+        })
+
+        return () => {socket.off()}
+    }, [users])
+
     useEffect( ()=>{
+        getUsers(receptor)
         getMensajes()
     },[])
 
     //procedimineto para obtener todos los usuarios
     const getMensajes = async () => {
+        const res = await axios.get(URI)
+        setMensajes(res.data)
         const res2 = await axios.get(URI+"fecha")
         setMensajesDESC(res2.data)
     }
 
+    const getUsers = async (rec) => {
+        const res = await axios.get(URIUsers)
+        setUsers(res.data)
+        users.map((user) => {
+            if(user.nombre===rec){
+                if(user.online){
+                    document.getElementById("divOnline").classList.add("mostrarOnline");
+                    document.getElementById("divOffline").classList.remove("mostrarOnline");
+                    document.getElementById("divOffline").classList.add("ocultar");
+                } else {
+                    document.getElementById("divOnline").classList.remove("mostrarOnline");
+                    document.getElementById("divOnline").classList.add("ocultar");
+                    document.getElementById("divOffline").classList.add("mostrarOnline");
+                }
+            }
+        })
+    }
     
     useEffect(() => {
         divRef.current.scrollIntoView({ behavior: 'smooth' })
@@ -73,20 +86,21 @@ const Chat = ({nombre}) => {
         setMensaje('')
     }
 
-    const getMensajesOnePerson = async (rec) => {
+    /* const getMensajesOnePerson = async (rec) => {
         if(receptor !== '') {
             const res = await axios.get(URI+'nombre_usuario_receptor/'+rec+"/"+nombre)
             setMensajes(res.data)
             const res2 = await axios.get(URI+'fecha')
             setMensajesDESC(res2.data)
-            document.getElementById("panelChat").classList.add("mostrar");
         }
-    }
+    } */
 
     async function showChat(rec){
+        getUsers(rec)
         setReceptor(rec)
         document.getElementById('labelNameUser').innerHTML=rec
-        getMensajesOnePerson(rec)
+        getMensajes()
+        document.getElementById("panelChat").classList.add("mostrar");
     }
 
     function doButton(){
@@ -160,48 +174,51 @@ const Chat = ({nombre}) => {
         let nombreAnterior = ''
 
         mensajes.map((mensaje) => {
-            let d = new Date(mensaje.fecha_envio)
-            let s = d.getFullYear()+"-"+d.getMonth()+"-"+d.getDay()+" "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
-            if(mensaje.nombre_usuario_emisor === nombre){
-                if(nombre === nombreAnterior){
-                    message.push(
-                        <div className='bloqueMensajeMio'>
-                            <div className='mensajeMio'>
-                                <p>{mensaje.mensaje} <small>{s}</small></p>
+            
+            if((mensaje.nombre_usuario_emisor === nombre && mensaje.nombre_usuario_receptor === receptor) || (mensaje.nombre_usuario_emisor === receptor && mensaje.nombre_usuario_receptor === nombre)) {
+                let d = new Date(mensaje.fecha_envio)
+                let s = d.getFullYear()+"-"+d.getMonth()+"-"+d.getDay()+" "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+                if(mensaje.nombre_usuario_emisor === nombre){
+                    if(nombre === nombreAnterior){
+                        message.push(
+                            <div className='bloqueMensajeMio'>
+                                <div className='mensajeMio'>
+                                    <p>{mensaje.mensaje} <small>{s}</small></p>
+                                </div>
                             </div>
-                        </div>
-                    )
+                        )
+                    } else {
+                        message.push(
+                            <div className='bloqueMensajeMio'>
+                                <p className='nombreMio'><b>{nombre}</b></p>
+                                <div className='mensajeMio'>
+                                    <p>{mensaje.mensaje} <small>{s}</small></p>
+                                </div>
+                            </div>
+                        )
+                    }
                 } else {
-                    message.push(
-                        <div className='bloqueMensajeMio'>
-                            <p className='nombreMio'><b>{nombre}</b></p>
-                            <div className='mensajeMio'>
-                                <p>{mensaje.mensaje} <small>{s}</small></p>
+                    if(mensaje.nombre_usuario_emisor === nombreAnterior){
+                        message.push(
+                            <div className='bloqueMensajeOtro'>
+                                <div className='mensajeOtro'>
+                                    <p>{mensaje.mensaje} <small>{s}</small></p>
+                                </div>
                             </div>
-                        </div>
-                    )
+                        )
+                    } else {
+                        message.push(
+                            <div className='bloqueMensajeOtro'>
+                                <p className='nombreOtro'><b>{mensaje.nombre_usuario_emisor}</b></p>
+                                <div className='mensajeOtro'>
+                                    <p>{mensaje.mensaje} <small>{s}</small></p>
+                                </div>
+                            </div>
+                        )
+                    }
                 }
-            } else {
-                if(mensaje.nombre_usuario_emisor === nombreAnterior){
-                    message.push(
-                        <div className='bloqueMensajeOtro'>
-                            <div className='mensajeOtro'>
-                                <p>{mensaje.mensaje} <small>{s}</small></p>
-                            </div>
-                        </div>
-                    )
-                } else {
-                    message.push(
-                        <div className='bloqueMensajeOtro'>
-                            <p className='nombreOtro'><b>{mensaje.nombre_usuario_emisor}</b></p>
-                            <div className='mensajeOtro'>
-                                <p>{mensaje.mensaje} <small>{s}</small></p>
-                            </div>
-                        </div>
-                    )
-                }
+                nombreAnterior = mensaje.nombre_usuario_emisor
             }
-            nombreAnterior = mensaje.nombre_usuario_emisor
         })
 
         return (message)
@@ -213,7 +230,17 @@ const Chat = ({nombre}) => {
                 {doButton()}
             </div>
             <div className='panelChat ocultar' id='panelChat'>
-                <div className='divNameUser'><div id="labelNameUser"></div><div className='ocultar' id='online'></div><div id='offline'></div></div>
+                <div className='divNameUser'>
+                    <div id="labelNameUser"></div>
+                    <div id="divOnline" className='ocultar'>
+                        <div id='online'></div>
+                        Online
+                    </div>
+                    <div id="divOffline" className='ocultar'>
+                        <div id='offline'></div>
+                        Offline
+                    </div>
+                </div>
                 <div className='chat'>
                     {doMessage()}
                     <div ref={divRef} id="idRef"></div>
