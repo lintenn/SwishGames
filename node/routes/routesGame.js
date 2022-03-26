4710c7d6eadcf2eca6af91e0fa4aa755c9bc280b
Luis <linten42@gmail.com>
2022-03-26 02:32:34 +0100

    Update search in main WIP

--- node/routes/routesGame.js
+++ node/routes/routesGame.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createGame, deleteGame, getAllGames, getGame, updateGame } from '../controllers/GameController.js'
+import { createGame, deleteGame, getAllGames, getGame, getSearchedGames, updateGame } from '../controllers/GameController.js'
 const router = express.Router()
 
 router.get('/', getAllGames)
 router.get('/:id', getGame)
+router.get('/buscar/:tit', getSearchedGames)
 router.post('/', createGame)
 router.put('/:id', updateGame)
 router.delete('/:id', deleteGame)
