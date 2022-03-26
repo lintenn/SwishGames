5b1281aa3c1f2c08800f6f4defe603bcdaff50f8
Luis <linten42@gmail.com>
2022-03-26 14:21:57 +0100

    Update main with footer, search WIP

--- frontend-react/src/vistas/main/main.js
+++ frontend-react/src/vistas/main/main.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { isAuthorized } from '../../helper/isAuthorized.js';
 import Swal from 'sweetalert2';
 import logo from '../../static/SwishGamesLogo.png';
+import logoSinLetras from '../../static/SwishGamesLogo_sin_letras.png';
 
 // import '../login/login.css'
 import './main.css';
@@ -167,7 +168,7 @@ const Main = () => {
             id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active"
+                <a className="nav-link active navSelected"
                   aria-current="page"
                   href=""><i className="fa-solid fa-gamepad"></i> Juegos</a>
               </li>
@@ -211,8 +212,35 @@ const Main = () => {
         <div className="col-lg-8 list-group">
           {doGames()}
         </div>
-      </main>
 
+        <div className="container bg-light">
+          <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <div className="col-md-4 d-flex align-items-center px-3">
+              <span className="text-muted">© 2022 SwishGames, Inc</span>
+            </div>
+
+            <a href="/"
+              className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+              <img className="bi me-2"
+                width="32"
+                height="32"
+                src={logoSinLetras}></img>
+            </a>
+
+            <ul className="nav col-md-4 justify-content-end list-unstyled d-flex px-3">
+              <li className="nav-item"><a href="#"
+                className="nav-link px-2 text-muted">Home</a></li>
+              <li className="nav-item"><a href="#"
+                className="nav-link px-2 text-muted">Contact</a></li>
+              <li className="nav-item"><a href="#"
+                className="nav-link px-2 text-muted">FAQs</a></li>
+              <li className="nav-item"><a href="#"
+                className="nav-link px-2 text-muted">About</a></li>
+            </ul>
+          </footer>
+        </div>
+
+      </main>
 
     </body>
 
