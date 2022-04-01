2f2182249743cfb0d51858902a579f0872fc125d
Ángel Joaquín Rodríguez Mercado <angelrodriguezmercado95@gmail.com>
2022-04-02 01:04:47 +0200

    Add ParticipantsGroups in backend

--- node/routes/routesGroup.js
+++ node/routes/routesGroup.js
@@ -1,11 +1,11 @@
 const express = require( 'express' );
-const GroupController = require( '../controllers/GroupController.js' );
+const ParticipantsGroupsController = require( '../controllers/ParticipantsGroupsController.js' );
 const router = express.Router();
 
-router.get( '/', GroupController.getAllGroups );
-router.get( '/:id', GroupController.getGroup );
-router.post( '/', GroupController.createGroup );
-router.put( '/:id', GroupController.updateGroup );
-router.delete( '/:id', GroupController.deleteGroup );
+router.get( '/', ParticipantsGroupsController.getAllParticipantsGroups );
+router.get( '/:id', ParticipantsGroupsController.getParticipantsGroups );
+router.post( '/', ParticipantsGroupsController.createParticipantsGroups );
+router.put( '/:id', ParticipantsGroupsController.updateParticipantsGroups );
+router.delete( '/:id', ParticipantsGroupsController.deleteParticipantsGroups );
 
 module.exports = router;
