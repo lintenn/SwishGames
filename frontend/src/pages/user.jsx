f9b42806ed290f04c1ea5b93787fbc21c1a4749e
AngeloGC <0619974249@uma.es>
2022-04-09 23:48:27 +0200

    User template upgraded!

--- frontend/src/pages/user.jsx
+++ frontend/src/pages/user.jsx
@@ -6,6 +6,8 @@ import socket from '../components/chat/Socket';
 import { Header } from '../components/header.jsx';
 import { Footer } from '../components/footer.jsx';
 import { Global } from '../helper/Global.js';
+import '../styles/user.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const User = () => {
 
@@ -56,14 +58,94 @@ const User = () => {
         } }
       />
       <main className="row justify-content-center mt-5">
-        <div className="col-3">
-          <br></br>
-          <br></br>
-          <h1>{name}</h1>
-          <h5>Descripción: {description}</h5>
-          <h5>Email: {email}</h5>
-          <h5>Fecha de nacimiento: {birthDate}</h5>
-          <h5>Fecha de creación: {creationDate}</h5>
+        <div class="container mt-5 ms-6">
+      
+            <div class="row gutters-sm">
+              <div class="col-md-2">
+                <div class="card" style={{width: '18rem;'}}>
+                  <img src="https://www.personality-database.com/profile_images/183486.png" class="px-5 pt-3 rounded-circle" alt="..."/>
+                  <div class="card-body">
+                    <p class="text-center fs-3 fw-bolder">{name}</p>
+                    <p class="text-center">{description}</p>
+                  </div>
+                </div>
+                <ul class="list-group list-group-flush border mt-3">
+                  <li class="list-group-item">
+                    <div class="row ms-1">
+                      <div class="col">
+                        <p class="text-end fs-3 fw-bolder">50</p>
+                      </div>
+                      <div class="col-8">
+                        <p class="text">Videojuegos completados</p>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="list-group-item">
+                    <div class="row ms-1">
+                      <div class="col">
+                        <p class="text-end fs-3 fw-bolder">69</p>
+                      </div>
+                      <div class="col-8">
+                        <p class="text">Videojuegosㅤㅤpor jugar</p>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-md-8">
+                
+                <div class="card mb-3">
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col-sm-3">
+                        Nombre
+                      </div>
+                      <div class="col-sm-9 text-secondary">{name}</div>
+                    </div>
+                    <hr/>
+                    <div class="row">
+                      <div class="col-sm-3">
+                        Email
+                      </div>
+                      <div class="col-sm-9 text-secondary">{email}</div>
+                    </div>
+                    <hr/>
+                    <div class="row">
+                      <div class="col-sm-3">
+                        Fecha de nacimiento
+                      </div>
+                      <div class="col-sm-9 text-secondary">{birthDate}</div>
+                    </div>
+                    <hr/>
+                    <div class="row">
+                      <div class="col-sm-3">
+                        Fecha de creación de la cuenta
+                      </div>
+                      <div class="col-sm-9 text-secondary">{creationDate}</div>
+                    </div>
+                    <hr/>
+                    <div class="row">
+                      <div class="col-sm-3">
+                        Privacidad
+                      </div>
+                      <div class="col-sm-9 text-secondary">uwu</div>
+                    </div>
+                    <hr/>
+                    <div class="row">
+                      <div class="col-sm-12">
+                        <button className="btn btn-outline-dark m-1">
+                          <i className="fa-solid fa-user"></i> Editar perfil
+                        </button>
+                        <button className="btn btn-outline-dark m-1">
+                          <i class="fa-solid fa-key"></i> Cambiar contraseña
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
         </div>
         <Footer/>
       </main>
