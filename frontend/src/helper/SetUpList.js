bd01e8aa94beca0342dee3b7167833b45a1d8969
Galo <galopg2001@hotmail.com>
2022-04-18 22:04:05 +0200

    Minor changes

--- frontend/src/helper/SetUpList.js
+++ frontend/src/helper/SetUpList.js
@@ -5,22 +5,23 @@ import Swal from 'sweetalert2';
 const baseUrl = Global.baseUrl;
 
 export const setUpList = ( listId, setList, setGames ) => {
-    
-      Swal.showLoading();
 
-      axios.get( `${baseUrl}lists/${listId}/` )
-         .then( res => {
+  Swal.showLoading();
 
-            setList( res.data );
+  axios.get( `${baseUrl}lists/${listId}/` )
+    .then( res => {
 
-         }).catch( err => console.log(err) )
-    
-      axios.get( `${baseUrl}contentsLists/${listId}/` )
-         .then( res => {
-         
-            setGames( res.data );
-         
-         }).catch( err => console.log(err) )
-  
-     Swal.close();
+      setList( res.data );
+
+    }).catch( err => console.log( err ) );
+
+  axios.get( `${baseUrl}contentsLists/${listId}/` )
+    .then( res => {
+
+      setGames( res.data );
+
+    }).catch( err => console.log( err ) );
+
+  Swal.close();
+
 };
