2ce011de8fc241970201b01cf11c8fd13f498f5f
Miguel Oña Guerrero <miguelog3295@outlook.com>
2022-04-21 10:27:07 +0200

    Add rating stars

--- frontend/src/pages/game.jsx
+++ frontend/src/pages/game.jsx
@@ -2,7 +2,8 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { isAuthorized } from '../helper/isAuthorized.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/game.css';
 import socket from '../components/chat/Socket';
 import { Header } from '../components/header.jsx';
 import { Footer } from '../components/footer.jsx';
@@ -49,12 +50,29 @@ const Game = () => {
 
         } }
       />
+
+
       <main className="row justify-content-center mt-5">
         <div className="col-2">
+
           <br></br>
           <br></br>
           <h1 >Game</h1>
           <h5>Titulo: {game.titulo}</h5>
+
+          <div class="rate">
+            <input type="radio" id="star5" name="rate" value="5" />
+            <label for="star5" title="5 estrellas">5 stars</label>
+            <input type="radio" id="star4" name="rate" value="4" />
+            <label for="star4" title="4 estrellas">4 stars</label>
+            <input type="radio" id="star3" name="rate" value="3" />
+            <label for="star3" title="3 estrellas">3 stars</label>
+            <input type="radio" id="star2" name="rate" value="2" />
+            <label for="star2" title="2 estrellas">2 stars</label>
+            <input type="radio" id="star1" name="rate" value="1" />
+            <label for="star1" title="1 estrella">1 star</label>
+          </div>
+
         </div>
         <Footer/>
       </main>
