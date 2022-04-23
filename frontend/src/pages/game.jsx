97df69c38872da00971c3bf9b38782c477003b01
Miguel Oña Guerrero <miguelog3295@outlook.com>
2022-04-24 00:05:52 +0200

    Add title and button

--- frontend/src/pages/game.jsx
+++ frontend/src/pages/game.jsx
@@ -54,47 +54,19 @@ const Game = () => {
 
 
       <main className="row justify-content-center mt-5">
-        <div className="col-2">
 
-          <br></br>
-          <br></br>
-          <h1 >Game</h1>
-          <h5>Titulo: {game.titulo}</h5>
+        <div className="container col-8 mt-5 border card">
 
-          <div className="rate">
-            <input type="radio"
-              id="star5"
-              name="rate"
-              value="5" />
-            <label htmlFor="star5"
-              title="5 estrellas">5 stars</label>
-            <input type="radio"
-              id="star4"
-              name="rate"
-              value="4" />
-            <label htmlFor="star4"
-              title="4 estrellas">4 stars</label>
-            <input type="radio"
-              id="star3"
-              name="rate"
-              value="3" />
-            <label htmlFor="star3"
-              title="3 estrellas">3 stars</label>
-            <input type="radio"
-              id="star2"
-              name="rate"
-              value="2" />
-            <label htmlFor="star2"
-              title="2 estrellas">2 stars</label>
-            <input type="radio"
-              id="star1"
-              name="rate"
-              value="1" />
-            <label htmlFor="star1"
-              title="1 estrella">1 star</label>
+          <div class="d-flex justify-content-between mt-3">
+            <h2 className='fw-bold ms-2'>{game.titulo}</h2>
+            
+            <button className="btn btn-outline-dark me-2 mb-3">
+              <i className="fa fa-plus-circle"></i> Añadir a lista
+            </button>
           </div>
 
         </div>
+
         <Footer/>
       </main>
     </div>
