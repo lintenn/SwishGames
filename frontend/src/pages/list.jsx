61fd5948941b808ba067070be951f4ddb375e34b
Miguel Oña Guerrero <miguelog3295@outlook.com>
2022-05-18 11:40:14 +0200

    Minor fixes

--- frontend/src/pages/list.jsx
+++ frontend/src/pages/list.jsx
@@ -151,7 +151,7 @@ const List = () => {
                     </div>
                     : <div></div>}
                   </div>
-                  
+
                 </div>
                 
                 
