cfc55509e9b65d346a107b2ec127a102a5da73bd
AngeloGC <0619974249@uma.es>
2022-05-21 23:46:07 +0200

    Show number of favourites on user's view

--- backend/routes/routesContentsLists.js
+++ backend/routes/routesContentsLists.js
@@ -6,6 +6,7 @@ router.get('/', ContentsListsController.getAllContentsLists);
 router.get('/:id_lista', ContentsListsController.getContentsListsByList);
 router.get('/:id_lista/buscar/:titulo', ContentsListsController.getSearchedContentsListsByList);
 router.get('/favoritos/:nombre_usuario', ContentsListsController.getFavoritesContentsListsByUser);
+router.get('/count/favoritos/:nombre_usuario', ContentsListsController.getFavoritesCountByUser);
 router.post('/', ContentsListsController.createContentsLists);
 router.put('/:id', ContentsListsController.updateContentsLists);
 router.delete('/:id_lista/:id_juego', ContentsListsController.deleteContentsLists);
