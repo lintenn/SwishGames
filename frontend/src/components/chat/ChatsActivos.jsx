39c959252e4515514da6970ac78bacdf24db7992
Ángel Joaquín Rodríguez Mercado <angelrodriguezmercado95@gmail.com>
2022-05-22 03:28:06 +0200

    Add remove message

--- frontend/src/components/chat/ChatsActivos.jsx
+++ frontend/src/components/chat/ChatsActivos.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Input from '@material-ui/core/Input';
 import PropTypes from 'prop-types';
 import socket from './Socket';
