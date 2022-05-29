626b20ef9d79b25d2280a8451c68c7aee1569925
Luis <linten42@gmail.com>
2022-05-29 23:26:26 +0200

    Update lists (detect a list's name that already exists does not work)

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -8,7 +8,8 @@ export const Footer = () => {
     <div className="container-fluid bg-light fixed-bottom mat-shadow tamañoFooter">
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-1 my-1">
         <div className="col-md-4 d-flex align-items-center px-3">
-          <span className="text-muted" lang='en'>© 2022 SwishGames, Inc</span>
+          <span className="text-muted"
+            lang="en">© 2022 SwishGames, Inc</span>
         </div>
         <NavLink to="/"
           className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
@@ -20,13 +21,13 @@ export const Footer = () => {
         </NavLink>
         <ul className="nav col-md-4 justify-content-end list-unstyled d-flex px-3">
           <li className="nav-item"><a href="#Home"
-            className="nav-link px-2 text-muted"><span lang='en'>Home</span></a></li>
+            className="nav-link px-2 text-muted"><span lang="en">Home</span></a></li>
           <li className="nav-item"><a href="#Contact"
-            className="nav-link px-2 text-muted"><span lang='en'>Contact</span></a></li>
+            className="nav-link px-2 text-muted"><span lang="en">Contact</span></a></li>
           <li className="nav-item"><a href="#FAQs"
-            className="nav-link px-2 text-muted"><span lang='en'>FAQs</span></a></li>
+            className="nav-link px-2 text-muted"><span lang="en">FAQs</span></a></li>
           <li className="nav-item"><a href="#About"
-            className="nav-link px-2 text-muted"><span lang='en'>About</span></a></li>
+            className="nav-link px-2 text-muted"><span lang="en">About</span></a></li>
         </ul>
       </footer>
     </div>
