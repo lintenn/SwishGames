fa6add07c759ceda9e4a4cca3b296b8fc9e7a909
Ángel Joaquín Rodríguez Mercado <angelrodriguezmercado95@gmail.com>
2022-05-31 10:56:31 +0200

    Add test formatDateTest

---
+++ frontend/src/test/chat/unitary/formatDateTest.test.js
@@ -0,0 +1,13 @@
+import { formatDate } from '../../../components/chat/format/formatDate.js';
+
+describe( 'formatDateTest', () => {
+
+  /* test( 'prueba', () => {
+
+    const date = new Date( '2020-01-01T00:00:00.000Z' );
+
+    expect( formatDate( date ) ).toBe( '1-1-2020 1:00' );
+
+  }); */
+
+});
