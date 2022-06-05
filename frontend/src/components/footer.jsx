b8b4e5133298d726e0555347494dae0fe3c5006e
Luis <linten42@gmail.com>
2022-06-05 18:04:12 +0200

    Fix editlist and accesibility contrast errors

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -8,7 +8,7 @@ export const Footer = () => {
     <div className="container-fluid bg-light fixed-bottom mat-shadow tamañoFooter">
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-1 my-1">
         <div className="col-md-4 d-flex align-items-center px-3">
-          <span className="text-muted"
+          <span className="greytext"
             lang="en">© 2022 SwishGames, Inc</span>
         </div>
         <NavLink to="/"
@@ -21,13 +21,13 @@ export const Footer = () => {
         </NavLink>
         <ul className="nav col-md-4 justify-content-end list-unstyled d-flex px-3">
           <li className="nav-item"><a href="/"
-            className="nav-link px-2 text-muted"><span lang="es">Inicio</span></a></li>
+            className="nav-link px-2 greytext"><span lang="es">Inicio</span></a></li>
           <li className="nav-item"><a href="#Contact"
-            className="nav-link px-2 text-muted"><span lang="es">Contacto</span></a></li>
+            className="nav-link px-2 greytext"><span lang="es">Contacto</span></a></li>
           <li className="nav-item"><a href="#FAQs"
-            className="nav-link px-2 text-muted"><span lang="en">FAQs</span></a></li>
+            className="nav-link px-2 greytext"><span lang="en">FAQs</span></a></li>
           <li className="nav-item"><a href="#About"
-            className="nav-link px-2 text-muted"><span lang="es">Acerca de</span></a></li>
+            className="nav-link px-2 greytext"><span lang="es">Acerca de</span></a></li>
         </ul>
       </footer>
     </div>
