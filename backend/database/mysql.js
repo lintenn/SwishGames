d61ffb341fd679a03c38cd5a0d18bca8b9f07608
Ángel Joaquín Rodríguez Mercado <angelrodriguezmercado95@gmail.com>
2022-06-08 01:29:06 +0200

    Add environment backend test

--- backend/database/mysql.js
+++ backend/database/mysql.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize')
 
-const mysql = new Sequelize('u325099778_SwishGames','u325099778_SwishGames','SwishGames A A G L M 5',{
+const { SWISHGAMES_NAME, SWISHGAMES_TEST_NAME, NODE_ENV } = process.env
+
+const nameBBDD = NODE_ENV === 'test' ? SWISHGAMES_TEST_NAME : SWISHGAMES_NAME
+
+const mysql = new Sequelize(nameBBDD,nameBBDD,'SwishGames A A G L M 5',{
     host:'sql168.main-hosting.eu',
     dialect: 'mysql'
 } )
